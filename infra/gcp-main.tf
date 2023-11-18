f8e90d102195f8682e3f2cbbd9d1af083568245e
Jeferson Costa <rc.jeferson@gmail.com>
2023-11-18 11:51:22 -0300

    feat: artifact separation

--- infra/gcp-main.tf
+++ infra/gcp-main.tf
@@ -1,7 +1,13 @@
-# Cria uma VM no Google Cloud
-resource "google_artifact_registry_repository" "spotmusic" {
+resource "google_artifact_registry_repository" "spotmusic-frontend" {
   location      = var.region
-  repository_id = "spotmusic"
+  repository_id = "spotmusic-frontend"
+  description   = "Docker images"
+  format        = "DOCKER"
+}
+
+resource "google_artifact_registry_repository" "spotmusic-backend" {
+  location      = var.region
+  repository_id = "spotmusic-backend"
   description   = "Docker images"
   format        = "DOCKER"
 }
